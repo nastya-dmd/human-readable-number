00fb9c4260ec2d5c17d74776314c9c8bd9f5a837
Anastasiya Demidchik <iskhakova.anastasiya@gmail.com>
2023-01-16 11:04:32 +0100

    feat: add code js for task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function toReadable(number) {
   
-    const numbersStringArray = [];
+    const numbersArray = [];
 
     const storageNumbers = {
         0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine',
@@ -8,36 +8,33 @@ module.exports = function toReadable(number) {
         20: 'twenty', 30: 'thirty', 40: 'forty', 50: 'fifty', 60: 'sixty', 70: 'seventy', 80: 'eighty', 90: 'ninety'
     };
 
-    if (number === 0) {
-        numbersStringArray.push(storageNumbers[0])
-    }
+   if (number === 0) {
+    numbersArray.push(storageNumbers[0]);
+   }
 
-    const numberHundreds = (number - number % 100) / 100;
-    if (numberHundreds > 0) {
+   const numberHundred = (number - number % 100) / 100;
+   
+   if (numberHundred > 0){
+    numbersArray.push(storageNumbers[numberHundred] + ' hundred');
+   }
 
-        numbersStringArray.push(storageNumbers[numberHundreds] + ' hundred')
+   const newNumber = number - (numberHundred * 100);
 
-    }
-
-    const newNumber = number - (numberHundreds * 100);
-
-    if (newNumber <= 19 && newNumber > 0) {
-        numbersStringArray.push(storageNumbers[newNumber]);
-      
-    }
-
-    if (newNumber > 19 && newNumber > 0) {
-        const remainderNewNumber = newNumber % 10;
-        const differenceNumber = newNumber - remainderNewNumber;
-        numbersStringArray.push(storageNumbers[differenceNumber])
-       if (remainderNewNumber !=0 ) {
-        numbersStringArray.push(storageNumbers[remainderNewNumber])
-    }
-       
-    }
+   if (newNumber > 19 && newNumber > 0) {
+    const remainderNewNumber = newNumber % 10;
+    const differenceNumbers = newNumber - remainderNewNumber;
+    numbersArray.push(storageNumbers[differenceNumbers]);
+    if (remainderNewNumber !=0) {
+        numbersArray.push(storageNumbers[remainderNewNumber]);
     
+      }
+  }
 
-    return numbersStringArray.join(' ');
+  if (newNumber <=19 && newNumber > 0) {
+    numbersArray.push(storageNumbers[newNumber]);
+  }
+
+   return numbersArray.join(' ')
 
 }
 
